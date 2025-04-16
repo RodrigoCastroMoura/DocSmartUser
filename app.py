0202d3d534248df6f1f785f6c945014cc03e1306
RodrigoCastroMoura <rodrigo_castro_moura@hotmail.com>
2025-04-16 14:31:48 -0300

    ajuste na URl

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ ALLOWED_EXTENSIONS = {
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # API endpoints
-API_BASE_URL = "http://127.0.0.1:8000/api"
+API_BASE_URL = "https://doc-smart-api-rodrigocastromo.replit.app/api"
 LOGIN_URL = f"{API_BASE_URL}/auth/login"
 LOGOUT_URL = f"{API_BASE_URL}/auth/logout"
 REFRESH_URL = f"{API_BASE_URL}/auth/refresh"
